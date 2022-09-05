d64126bf5b0f5bb0799e15ae83c32779192a62df
Josuah Demangeon <me@josuah.net>
2022-09-05 18:08:12 +0200

    @

--- firmware.c
+++ firmware.c
@@ -1,9 +1,22 @@
 /* Send requests to a Wishbone B4 bus as master through SPI */
 
 #include "pico/stdlib.h"
+#include "pico/stdio.h"
 #include "hardware/spi.h"
 #include "hardware/gpio.h"
 #include "pico-ice/flash.h"
+#include <stdio.h>
+#include <string.h>
+
+void
+print_page(uint8_t const *page)
+{
+	for (size_t i = 0; i < FLASH_PAGE_SIZE; i++) {
+		printf(" %02X", page[i]);
+		if ((i + 1) % 32 == 0)
+			printf("\n");
+	}
+}
 
 int
 main(void)
@@ -12,14 +25,25 @@ main(void)
 
 	stdio_init_all();
 
+	gpio_init(25);
+	gpio_set_dir_out_masked(1 << 25);
+	gpio_put(25, true);
+
 	spi_init(spi_default, 1000 * 1000);
-	gpio_set_function(PICO_DEFAULT_SPI_CSN_PIN, GPIO_FUNC_SPI);
 	gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
 	gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
 	gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
+	gpio_init(PICO_DEFAULT_SPI_CSN_PIN);
+	gpio_put(PICO_DEFAULT_SPI_CSN_PIN, 1);
+	gpio_set_dir(PICO_DEFAULT_SPI_CSN_PIN, GPIO_OUT);
 
-	flash_program_page(spi_flash, 0x000000, page);
-	flash_read(spi_flash, 0x000000, page, sizeof page);
+	memset(page, 0xFF, sizeof page);
+
+	flash_program_page(spi_default, 0x000000, page);
+	for (;;) {
+		flash_read_page(spi_default, 0x000000, page);
+		print_page(page);
+	}
 
 	return 0;
 }
