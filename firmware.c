28d30ecaf29587fa69d5ef405559c5b28a196848
Josuah Demangeon <me@josuah.net>
2022-09-19 17:40:24 +0200

    make user of the newer SDK API

--- firmware.c
+++ firmware.c
@@ -2,10 +2,19 @@
 #include "pico/stdio.h"
 #include "pico_ice/ice.h"
 
+/* TinyUSB includes */
+#include "bsp/board.h"
+#include "tusb.h"
+
 int
 main(void)
 {
     stdio_init_all();
-    ice_init_fpga_clock(8);
+    ice_init_defaults();
+
+    for (;;) {
+        ice_usb_task();
+    }
+
     return 0;
 }
